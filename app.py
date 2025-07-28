2618f2c571804441d9bcd3faa1a174d96e4a5eca
DaniarRifqi <daniarrifqi2@gmail.com>
2025-07-29 03:11:41 +0700

    app run

--- app.py
+++ app.py
@@ -164,4 +164,4 @@ def uploaded_file(filename):
     return send_from_directory(uploads_dir, filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()
